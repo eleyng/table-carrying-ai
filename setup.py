bf27a884b5a1c130a468d5b143260c2e420fd1c6
Eley Ng <eleyng@stanford.edu>
2023-02-14 22:01:55 -0800

    added gif

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="cooperative-transport",
     version="0.0.1",
-    author="Eley Ng, Albert Li",
-    author_email="eleyng@stanford.edu, ahli@stanford.edu",
+    author="Eley Ng, Ziang Liu",
+    author_email="eleyng@stanford.edu",
     description="Custom environment and model for training RL agents for cooperative transport.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/eley-ng/cooperative-transport",
+    url="https://github.com/eleyng/table-carrying-ai",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
