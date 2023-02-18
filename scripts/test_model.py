480dd047e39f9621a6ee34d399d66bf8b0ac62e7
Eley Ng <eleyng@stanford.edu>
2023-02-18 03:26:44 -0800

    Cleaning unneccessary dependencies

--- scripts/test_model.py
+++ scripts/test_model.py
@@ -1,12 +1,8 @@
 import random
-import argparse
 import torch
 import gym
-import time
-import pygame
 import numpy as np
-import os
-from os.path import join, exists, isdir, isfile
+from os.path import join, isdir, isfile
 from os import mkdir, listdir
 
 import sys
@@ -15,11 +11,7 @@ import sys
 # sys.path.append("algo/planners")
 from algo.planners.cooperative_planner.models import VRNN
 
-from cooperative_transport.gym_table.envs.utils import load_cfg, init_joystick, debug_print, FPS, CONST_DT, MAX_FRAMESKIP, get_keys_to_action
-from cooperative_transport.gym_table.envs.custom_rewards import custom_reward_function
-from libs.planner.planner_utils import (
-    pid_single_step,
-)
+from cooperative_transport.gym_table.envs.utils import CONST_DT
 from libs.utils import play_hil_planner
 
 
@@ -27,141 +19,9 @@ from libs.utils import play_hil_planner
 from configs.experiment.experiment_config import get_experiment_args
 from configs.robot.robot_planner_config import get_planner_args
 # from configs.robot.robot_policy_config import get_policy_args
-from configs.human.human_config import get_human_args
 
 
 VERBOSE = False # Set to True to print debug info
-
-
-
-
-def play_traj(
-    env,
-    mode,
-    model=None,
-    optimizer=None,
-    train_stats=None,
-    SEQ_LEN=60,
-    H=15,
-    playback_trajectory=None,
-    n_steps=1000,
-    zoom=1,
-    fps=30,
-
-):
-
-    # record trajectory played
-    trajectory = {}
-    trajectory["states"] = []
-    trajectory["actions"] = []
-    trajectory["rewards"] = []
-
-    # reset environment
-    obs = env.reset()
-    done = False
-    running = True
-    n_iter = 0
-    success = False
-
-    # If playback trajectory is provided as the EGO actions (player 1's), use that instead of ego model
-    if mode == "playback_trajectory" or mode == "data_aug":
-        actions = playback_trajectory["actions"]
-        waypoints = playback_trajectory["states"]
-
-    next_game_tick = time.time()
-    clock = pygame.time.Clock()
-
-    # Main Game loop
-    while running:
-        loops = 0
-
-        if done:
-            time.sleep(1)
-            pygame.quit()
-            break
-        else:
-            while time.time() > next_game_tick and loops < MAX_FRAMESKIP and not done:
-                if mode == "playback_trajectory":
-                    if True:  # n_iter < actions.shape[0]:
-                        n_iter = min(n_iter, actions.shape[0] - 1)
-                        # pdb.set_trace()
-                        waypoint = waypoints[n_iter, :4]
-                        u_h = actions[n_iter, 2:]
-                        # debug_print("waypoint: ", waypoint)
-                        pid_actions = pid_single_step(
-                            env,
-                            waypoint,
-                            kp=0.2,
-                            ki=0.0,
-                            kd=0.0,
-                            max_iter=40,
-                            dt=CONST_DT,
-                            eps=1e-2,
-                            linear_speed_limit=[-2.0, 2.0],
-                            angular_speed_limit=[-np.pi / 4, np.pi / 4],
-                            u_h=u_h,
-                        )
-                        pid_actions = np.clip(pid_actions, -1.0, 1.0)
-                        debug_print(
-                            "actual robot action: ",
-                            actions[n_iter, :2],
-                            "pid action: ",
-                            pid_actions,
-                            "human action: ",
-                            actions[n_iter, 2:],
-                        )
-
-                        u_all = [
-                            pid_actions[0],
-                            pid_actions[1],
-                            actions[n_iter, 2],
-                            actions[n_iter, 3],
-                        ]
-
-                        debug_print("sending actions", u_all)
-
-                        # obs, reward, done, info = env.step(actions[n_iter, :])
-
-                        obs, reward, done, info = env.step(u_all)
-                        if done:
-                            running = False
-                            break
-                        trajectory["actions"].append(pid_actions)
-                        trajectory["states"].append(obs)
-
-                        # mse = ((actions[n_iter, :] - u_all) ** 2).mean(axis=0)
-                        # debug_print("MSE: ", mse)
-
-                # iterate to next step in game loop
-                debug_print("Loop: ", loops, "n_iter", n_iter, info)
-                next_game_tick += CONST_DT
-                loops += 1
-            n_iter += 1
-
-            # UPDATE DISPLAY
-            if not env.done:
-                env.redraw()
-                ##if callback is not None:
-                ##    callback(prev_obs, obs, action, rew, env_done, info)
-                # CLOCK TICK
-                clock.tick(FPS)
-                if clock.get_fps() > 0:
-                    debug_print("Reported dt: ", 1 / clock.get_fps())
-
-                # process pygame events
-                for event in pygame.event.get():
-                    # test events, set key states
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == 27:
-                            running = False
-                    elif event.type == pygame.QUIT:
-                        running = False
-
-    # Save trajectory
-    trajectory["states"] = np.stack(trajectory["states"], axis=0)
-    trajectory["actions"] = np.stack(trajectory["actions"], axis=0)
-
-    return trajectory, success
 
 
 def main(sysargv):
