06c27ada1c1c406a198e6581fa0ec5c15b540434
Eley Ng <eleyng@stanford.edu>
2023-04-29 22:54:14 -0700

    untested -- real robot scripts

---
+++ configs/experiment/real_experiment_config.py
@@ -0,0 +1,141 @@
+import argparse
+
+
+def get_experiment_args(parser):
+    parser.add_argument("--seed", type=int, default=88, help="Seed")
+
+    parser.add_argument(
+        "--robot_name_1", type=str, default="locobot1", help="Name of robot 1"
+    )
+    parser.add_argument(
+        "--robot_name_2", type=str, default="locobot2", help="Name of robot 2"
+    )
+
+    # ------------------------ EXPERIMENT SETTINGS ------------------------
+    parser.add_argument(
+        "--exp-name",
+        type=str,
+        default="test_p1-planner_p2-human-keyboard",
+        help="Name of experiment. Used to name saved trajs and plots.",
+    )
+    parser.add_argument(
+        "--run-mode",
+        type=str,
+        default="hil",
+        help="Run mode. Options: [hil | replay_traj | coplanning]",
+    )
+    parser.add_argument(
+        "--ep",
+        type=int,
+        default=0,
+        help="Episode number to start from when recording data.",
+    )
+
+    # ------------------------ ROBOT MODE ------------------------ #
+    parser.add_argument(
+        "--robot-mode",
+        type=str,
+        default="planner",
+        help="Robot mode. Options: [planner | policy | data]",
+    )
+    parser.add_argument(
+        "--planner-type",
+        type=str,
+        default="vrnn",
+        help="Planner type. Options: [vrnn | rrt | diffusion_policy].",
+    )
+    parser.add_argument(
+        "--policy-type",
+        type=str,
+        default="diffusion",
+        help="Policy type. Options: [diffusion | bc].",
+    )
+
+    # ------------------------ HUMAN MODE ------------------------ #
+    parser.add_argument(
+        "--human-mode",
+        type=str,
+        default="data",
+        help="Human algorithm. Options: [real | data | policy (TODO: add policy)]. \
+            If real or data, then must specify human control is keyboard or joystick.",
+    )
+    parser.add_argument(
+        "--human-control",
+        type=str,
+        default="joystick",
+        help="Human control. Options: [keyboard, joystick].",
+    )
+    parser.add_argument(
+        "--subject-id",
+        type=int,
+        default=0,
+        help="Subject ID. Used to load human data.",
+    )
+
+    # ------------------------ REWARD FUNCTION SETTINGS ------------------------ #
+    parser.add_argument(
+        "--include-interaction-forces-in-rewards",
+        action="store_true",
+        default=False,
+        help="Include interaction forces in reward function.",
+    )
+
+    # ------------------------ DISPLAY SETTINGS ------------------------ #
+    parser.add_argument(
+        "--render-mode",
+        type=str,
+        default="gui",
+        help="Render mode. Options: [gui | headless].",
+    )
+    parser.add_argument(
+        "--add-buffer",
+        action="store_true",
+        default=False,
+        help="Add buffer to display.",
+    )
+    parser.add_argument(
+        "--display-pred",
+        action="store_true",
+        default=False,
+        help="Display predicted trajectories in pygame window.",
+    )
+    parser.add_argument(
+        "--display-gt",
+        action="store_true",
+        default=False,
+        help="Display ground truth trajectories in pygame window.",
+    )
+    parser.add_argument(
+        "--display-past-states",
+        action="store_true",
+        default=False,
+        help="Display visited states as the game progesses in pygame window.",
+    )
+
+    # ------------------------ DIRECTORIES ------------------------ #
+    parser.add_argument(
+        "--results-dir", type=str, default="results", help="Results directory"
+    )
+    parser.add_argument("--plot-dir", type=str, default="plots", help="Plots directory")
+    parser.add_argument(
+        "--data-dir",
+        type=str,
+        default="datasets/real_test/simple_map/np_trajectories", #unseen_map", #"datasets/table-demos/table-demos_traj/test/test_holdout", # dp_test/strat_0",
+        help="Name of of dataset directory storing past trajectory rollouts; full path from base directory",
+    )
+    parser.add_argument(
+        "--map-dir",
+        type=str,
+        default="datasets/real_test/simple_map/map_cfg", #unseen_map", #"datasets/table-demos/table-demos_map-cfg/map_cfg_rnd_obstacle_v2", #dp_test/strat_0/map_cfg",
+        help="Name of directory storing past trajectory rollouts' map configurations; full path from base directory",
+    )
+    parser.add_argument(
+        "--map-config",
+        type=str,
+        default="datasets/real_test/real_test.yml", #3.yml", #"cooperative_transport/gym_table/config/maps/rnd_obstacle_v2.yml",
+        help="Map Config File Path -- stores possible map configurations for the environment, which you can define.",
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="Experiment Configuration.")
---
+++ libs/real_robot_utils.py
@@ -0,0 +1,184 @@
+
+import math
+import os
+import re
+import time
+from os.path import join
+
+import cv2
+import numpy as np
+import rospy
+from geometry_msgs.msg import PoseStamped, Twist, Vector3
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Empty
+
+from cooperative_transport.gym_table.envs.custom_rewards import \
+    custom_reward_function
+
+# -------------------------------------------- INIT GLOBAL REAL WORLD PARAMS -------------------------------------------- #
+odom_1 = None
+odom_2 = None
+robot_name_1 = "locobot1"
+robot_name_2 = "locobot2"
+if not rospy.has_param("/" + robot_name_1 + "/use_base") or rospy.get_param("/" + robot_name_1 + "/use_base") == False:
+    print("Error: robot name not found...")
+    exit(0)
+if not rospy.has_param("/" + robot_name_2 + "/use_base") or rospy.get_param("/" + robot_name_2 + "/use_base") == False:
+    print("Error: robot name not found...")
+    exit(0)
+
+# REAL WORLD PARAMETERS
+# For locobot point P control i.e. distance from robot control point to center:
+L_real = 0.13 # meters
+r1x = -L_real
+r1y = 0
+r2x = L_real
+r2y = 0
+# For map (all in meters) ## TODO: update these values
+MIN_X = -1.9
+MAX_X = 1.9
+MIN_Y = -1.2
+MAX_Y = 1.0
+
+
+def move(v_x=0, v_y=0, yaw=0, duration=1.0, curr_pub=None):
+    """ Move robot for a certain duration by publishing velocity command
+    v_x, v_y, yaw: desired velocity and yaw
+    duration: duration of movement
+    curr_pub: publisher to publish velocity command
+    """
+    time_start = rospy.get_time()
+    r = rospy.Rate(30)
+    while (rospy.get_time() < (time_start + duration)):
+        curr_pub.publish(Twist(linear=Vector3(x=v_x, y=v_y), angular=Vector3(z=yaw)))
+        r.sleep()
+    curr_pub.publish(Twist())
+
+def share_control(f1, f2, factor=0.1):
+    """ Return commanded desired table velocity given two forces applied on it
+    f1, f2: 2D np.array or np.mat, each (2, 1)
+    Return: desired table velocity (v_des,x v_des,y omega_z of table) np.array (3, 1)
+    """
+    def get_wrench(f1, f2):
+        """ Return wrench vector given two forces
+        f1, f2: 2D np.array or np.mat, each (2, 1)
+        Return: Wrench (total F_x, F_y, T_z applied on table) np.array (3, 1)
+        """
+        f1_mat = np.mat([
+            [1, 0],
+            [0, 1],
+            [-r1y, r1x]
+        ])
+        f2_mat = np.mat([
+            [1, 0],
+            [0, 1],
+            [-r2y, r2x]
+        ])
+
+        f1 = f1.reshape(2,1)
+        f2 = f2.reshape(2,1)
+
+        return f1_mat @ f1 + f2_mat @ f2
+    des_wrench = get_wrench(f1, f2)
+    des_vel = des_wrench * factor
+    return des_vel
+
+def euler_from_quaternion(x, y, z, w):
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+    
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+    
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+    
+    return roll_x, pitch_y, yaw_z # in radian
+
+def reset_odom():
+    reset_odom_1 = rospy.Publisher("/" + robot_name_1 + "/mobile_base/commands/reset_odometry", Empty, queue_size=10)
+    reset_odom_2 = rospy.Publisher("/" + robot_name_2 + "/mobile_base/commands/reset_odometry", Empty, queue_size=10)
+    # reset odometry (these messages take a few iterations to get through)
+    timer = time.time()
+    while time.time() - timer < 0.25:
+        reset_odom_1.publish(Empty())
+        reset_odom_2.publish(Empty())
+
+def natural_sort_key(s, _nsre=re.compile("([0-9]+)")):
+    return [int(text) if text.isdigit() else text.lower() for text in _nsre.split(s)]
+
+def make_video(dir, name):
+    """Make video from images in a folder.
+
+    Args:
+        dir (str): Path to folder containing images.
+        name (str): Name of video to save.
+    """
+    image_folder = dir
+    video_name = join(dir, f"{name}.avi")
+
+    images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+    sorted_images = sorted(images, key=natural_sort_key)
+    frame = cv2.imread(os.path.join(image_folder, sorted_images[0]))
+    height, width, layers = frame.shape
+
+    video = cv2.VideoWriter(video_name, 0, 1, (width, height))
+
+    for image in sorted_images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
+
+    cv2.destroyAllWindows()
+    video.release()
+
+def compute_reward(
+    states,
+    goal,
+    obs,
+    env=None,
+    vectorized=False,
+    interaction_forces=None,
+    u_r=None,
+    u_h=None,
+    collision=None,
+    collision_checking_env=None,
+    success=None,
+) -> float:
+    """
+    Compute reward for the given state and goal.
+
+    Args:
+        states (np.ndarray): shape (N, obs_dim). Evaluating the reward for each state
+                in the batch of size N.
+        goal (np.ndarray): shape (2, ). Goal position
+        obs (np.ndarray): shape (num_obs, 2). Obstacles positions for each obstacle
+        include_interaction_forces_in_reward (bool): If True, include the interaction forces in the reward
+        interaction_forces (float): If provided, use the interaction forces computed as a part of the reward fn
+        vectorized (bool): Whether to vectorize the reward computation.
+                In inference, this should be True since we want to sample from the model.
+    """
+    if env.include_interaction_forces_in_rewards:
+        reward = custom_reward_function(
+            states,
+            goal,
+            obs,
+            interaction_forces=interaction_forces,
+            vectorized=True,
+            collision_checking_env=collision_checking_env,
+            env=env,
+            u_h=u_h,
+        )
+    else:
+        reward = custom_reward_function(
+            states, goal, obs, vectorized=True, env=env, collision_checking_env=collision_checking_env, u_h=u_h
+        )
+    return reward
